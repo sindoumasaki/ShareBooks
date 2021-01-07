87a7dff7c26c3cdecc470e29a41706b719c6f07f
EC2 Default User <ec2-user@ip-172-31-65-170.ec2.internal>
2021-01-07 15:24:20 +0000

    [header作成]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   before_action :check_employer, only: %i[update destroy]
 
+  # 閲覧用ゲストユーザーの編集・削除を防ぐための記述
   def check_employer
     if resouce.email == 'employer@example.com'
       redirect_to user_homes_home_path
